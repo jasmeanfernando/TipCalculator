e2b45feb374fec3b64af3c43a947c27b8031c16f
Jasmean Fernando <jasmeanfernando@gmail.com>
2022-10-01 17:31:59 -0400

    Initial Commit

---
+++ Shared/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  Shared
+//
+//  Created by Jasmean Fernando on 10/1/22.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
